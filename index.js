226f11522e372cbad37d620bc81628fed5ae8956
Percy Córdova <cordova_inf@hotmail.com>
2024-08-23 18:32:43 -0500

    se creo configuraacion para la conexion de la bd

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express")
+const { dbConnection } = require("./database/config")
 require("dotenv").config()
 // Crear el servidor de express
 const app = express()
 
+//Base de datos
+dbConnection()
 
 //Escuchar peticiones en el puerto 4500
 app.listen(process.env.PORT, () => {
