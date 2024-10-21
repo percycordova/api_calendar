2cf2ed281e065eafa96cfc3723b39cbbf29cb275
Percy Córdova <cordova_inf@hotmail.com>
2024-10-21 16:24:21 -0500

    se culmina crud

--- controllers/calendarEventsController.js
+++ controllers/calendarEventsController.js
@@ -23,35 +23,94 @@ const createCalendarEvent = async (req = request, res = response) => {
 }
 
 const updateCalendarEvent = async (req = request, res = response) => {
+  const eventId = req.params.id
 
-  
+  try {
 
-  res.json({
-    ok: true,
-    msg: "updateCalendarEvent"
-  })
+    const event = await EventCalendar.findById(eventId)
+
+    if (!event) {
+      return res.status(404).json({
+        ok: false,
+        msg: "No existe el evento"
+      })
+    }
+
+    if (event.user.toString() !== req.uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No autorizado"
+      })
+    }
+
+    const newEvent = {
+      ...req.body,
+      user: req.uid
+    }
+
+    const eventUpdate = await EventCalendar.findByIdAndUpdate(eventId, newEvent, { new: true })
+
+    return res.json({
+      ok: true,
+      message: "Evento actualizado",
+      event: eventUpdate
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador"
+    })
+  }
+
 
 }
 
 const getCalendarEvents = async (req = request, res = response) => {
   //  Me trae los eventos con el campo nombre que viene de la tabla usuario 
   // const events = await EventCalendar.find().populate("user","name")
-
   const events = await EventCalendar.find().populate("user", "name")
-
   res.json({
     ok: true,
     events
   })
-
 }
 
 const deleteCalendarEvent = async (req = request, res = response) => {
 
-  res.json({
-    ok: true,
-    msg: "deleteCalendarEvent"
-  })
+  const eventId = req.params.id
+  try {
+
+    const event = await EventCalendar.findById(eventId)
+
+    if (!event) {
+      return res.status(404).json({
+        ok: false,
+        msg: "No existe el evento"
+      })
+    }
+
+    if (event.user.toString() !== req.uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No autorizado"
+      })
+    }
+
+    await EventCalendar.findByIdAndDelete(eventId)
+
+    res.json({
+      ok: true,
+      msg: "Evento eliminado"
+    })
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador"
+    })
+  }
 
 }
 
